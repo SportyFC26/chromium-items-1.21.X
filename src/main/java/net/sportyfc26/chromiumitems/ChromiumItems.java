ffb691aba819fa3f9f271280a1ffd79d74962e88
SportyFC26 <CIaranrclewis@icloud.com>
2025-08-03 23:05:56 +0100

    Added RegisterModItems method Created ModItems Class

--- src/main/java/net/sportyfc26/chromiumitems/ChromiumItems.java
+++ src/main/java/net/sportyfc26/chromiumitems/ChromiumItems.java
@@ -6,7 +6,7 @@ import net.sportyfc26.chromiumitems.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// Very important comment
+// Very important comment Hi
 public class ChromiumItems implements ModInitializer {
 	public static final String MOD_ID = "chromiumitems";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
