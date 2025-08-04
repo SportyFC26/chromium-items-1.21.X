1e940e72b49aa86113d4a682d173143c8750a8d6
SportyFC26 <CIaranrclewis@icloud.com>
2025-08-04 23:22:51 +0100

    Version Alpha 1.1
    Created a new ModItems Class
    Added a translation json
    Added json files for the two new items
    Created textures for Raw Chromium and Chromium Ingot

--- src/main/java/net/sportyfc26/chromiumitems/item/ModItems.java
+++ src/main/java/net/sportyfc26/chromiumitems/item/ModItems.java
@@ -1,5 +1,7 @@
 package net.sportyfc26.chromiumitems.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 import net.sportyfc26.chromiumitems.ChromiumItems;
@@ -8,13 +10,18 @@ import net.minecraft.registry.Registry;
 
 public class ModItems {
     public static final Item CHROMIUM_INGOT = registerItem( "chromium_ingot", new Item(new Item.Settings()));
+    public static final Item RAW_CHROMIUM = registerItem("raw_chromium", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(ChromiumItems.MOD_ID, name), item);
-
     }
 
     public static void registerModItems() {
         ChromiumItems.LOGGER.info("Registering Mod Items For " + ChromiumItems.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(CHROMIUM_INGOT);
+            entries.add(RAW_CHROMIUM);
+        });
     }
 }
